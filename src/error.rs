40537c35023901b7dcc6647a8fa93c1d196ad643
Erik Norfelt <enorfelt@outlook.com>
2025-09-20 12:21:38 +0200

    removed dep thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,37 @@
 use core::convert::Infallible;
-use thiserror::Error;
+use core::fmt;
 
 /// Result
 pub type Result<T, E = Error> = core::result::Result<T, E>;
 
 /// Error
-#[derive(Clone, Copy, Debug, Eq, Error, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Error {
-    #[error("invalid configuration register (resolution)")]
     ConfigurationRegister,
-    #[error("the bus was expected to be pulled high by a ~5K ohm pull-up resistor, but it wasn't")]
     NotHigh,
-    #[error("pin")]
-    Pin(#[from] Infallible),
-    #[error("family code mismatch")]
+    Pin(Infallible),
     MismatchedFamilyCode,
-    #[error("there are no devices attached to the 1-Wire bus")]
     NoAttachedDevices,
-    #[error("CRC mismatch {{ crc8={crc8} }}")]
     MismatchedCrc { crc8: u8 },
-    #[error("timeout expired")]
     Timeout,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::ConfigurationRegister => write!(f, "invalid configuration register (resolution)"),
+            Error::NotHigh => write!(f, "the bus was expected to be pulled high by a ~5K ohm pull-up resistor, but it wasn't"),
+            Error::Pin(_) => write!(f, "pin error"),
+            Error::MismatchedFamilyCode => write!(f, "family code mismatch"),
+            Error::NoAttachedDevices => write!(f, "there are no devices attached to the 1-Wire bus"),
+            Error::MismatchedCrc { crc8 } => write!(f, "CRC mismatch {{ crc8={} }}", crc8),
+            Error::Timeout => write!(f, "timeout expired"),
+        }
+    }
+}
+
+impl From<Infallible> for Error {
+    fn from(never: Infallible) -> Self {
+        Error::Pin(never)
+    }
 }
